9704df76318a3525ba706495aa44c34a472385f3
rebeccacofie <rebecca.cofie@ly.st>
2022-11-04 20:27:25 +0000

    Initial Commit

---
+++ That Girl/That_GirlApp.swift
@@ -0,0 +1,17 @@
+//
+//  That_GirlApp.swift
+//  That Girl
+//
+//  Created by Rebecca Cofie on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct That_GirlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
