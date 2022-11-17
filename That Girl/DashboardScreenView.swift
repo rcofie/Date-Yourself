3a14b743afa8f04c504835e7e2899ff99adb1655
rebeccacofie <rebecca.cofie@ly.st>
2022-11-17 09:43:38 +0000

    TabBar icons and file directory fixes

--- That Girl/DashboardScreenView.swift
+++ That Girl/DashboardScreenView.swift
@@ -6,39 +6,45 @@
 //
 
 import SwiftUI
+import UIKit
 
 
 struct TabViewScreen: View {
     init() {
         UITabBar.appearance().backgroundColor = UIColor(CustomColors.cream)
-        }
+//        UITabBarItem.appearance().setTitleTextAttributes([.font : TabViewScreen.karla], for: []) I want to change the font of the tabItems to Karla regular
+    }
     var body: some View {
         TabView {
             Text("Hobbies")
                 .tabItem {
+                    Image(systemName: "star")
                     Text("Hobbies")
-                        .font(Font.custom("Karla-Regular", size: 9))
                 }
             Text("Appearance")
                 .tabItem {
+                    Image(systemName: "mouth.fill")
                     Text("Appearance")
-                        .font(Font.custom("Karla-Regular", size: 9))
                 }
             DashboardScreenView()
                 .tabItem {
+                    Image(systemName: "house")
                     Text("Home")
                 }
             Text("Organisation")
                 .tabItem {
+                    Image(systemName: "calendar")
                     Text("Organisation")
                 }
             Text("Health")
                 .tabItem {
+                    Image(systemName: "bolt.heart")
                     Text("Health")
                 }
         }
+        .accentColor(.black)
     }
-}
+} // icons can be changed at a later date using custom ones
 
 struct DashboardScreenView: View {
     var body: some View {
